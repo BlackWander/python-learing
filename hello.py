c34cf7c7d56fd98e90e3a7948ceb85e77a8d220d
BlackWander <lutliu604@gmail.com>
2021-11-22 01:09:37 +0800

    new practice

--- hello.py
+++ hello.py
@@ -1,31 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-time=input('输入你每天的睡眠时间（h）：')
-t=int(time)
-if t>=8 and t<=10:
-   print('你这个年纪你还睡得着觉啊')
-elif t>10:
-    print('你是猪吗')
-else:
-    print('没救了，准备后事吧')
+import math
 
-input()
+def quadratic(a,b,c):
+    delta=(b**2-4*a*c)
+    if delta < 0:
+        return('无解')
+    else:
+        x1=(-b+math.sqrt(b**2-4*a*c))/2*a
+        x2=(-b-math.sqrt(b**2-4*a*c))/2*a
+        return (x1,x2)
 
-high=input('输入你的身高:')
-height=input('输入你的体重：')
-h1=float(high)
-h2=int(height)
-x=(h2)/h1**2
-print(f'你的BMI指数是：{x:.1f}')
-if x<18.5:
-    print('过轻')
-elif x>=18.5 and x<25:
-    print('正常')
-elif x>=25 and x<28:
-    print('过重')
-elif x>=28 and x<32:
-    print('肥胖')
-else:
-    print('严重肥胖')
-
+a,b,c=map(float,input('输入三个值,用空格键隔开：').split())
+x,y= quadratic(a,b,c)
+print(x,y)
